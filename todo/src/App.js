2cf08878e916beae292978f5b0d53870528a1b94
Saintlify <management@saintlify.com>
2023-02-02 11:32:24 -0600

    break down into components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import React from "react";
+import Filter from "./components/filters/Filters";
+import Form from "./components/form/Form";
+import Todo from "./components/todo/Todo";
 
-function App() {
+function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
-    <>
-    </>
+    <div className="todoapp stack-large">
+      <h1>To-Do</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <Filter />
+        <Filter />
+        <Filter />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
   );
 }
 
